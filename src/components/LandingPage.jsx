55f4ae9791ace48836f2cea3d7bf5a915e187f74
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-03-29 00:40:53 +0530

    Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,6 @@
+import { ArrowDown, Github, Linkedin, Mail } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const LandingPage = () => {
     return (
@@ -10,14 +12,30 @@ const LandingPage = () => {
                     </div>
                 ))}
                 <div className="mt-6">
-                    <h5>{`(SCROLL TO EXPLORE)`}</h5>
+                    <h5 className="flex gap-2">{`(SCROLL TO EXPLORE) `}<ArrowDown className="transition-[0.3s] repeat-infinite animate-bounce" /></h5>
                 </div>
             </div>
             <div className="border-t-2 border-zinc-200 mt-28"></div>
-            <div className="flex flex-row-reverse justify-between items-center px-8 py-6 lg:px-16">
-                {/* <h3>Hello</h3> */}
-                {/* <h3>World</h3> */}
-                <Button>Contact</Button>
+            <div className="flex justify-between items-center px-8 py-6 lg:px-16">
+                <div>
+                    <Link href="https://github.com/SudhanshuPanthri" target="_blank" className="flex justify-center items-center gap-4">
+                        <Github />
+                        <h3 className="hidden lg:block">Sudhanshu Panthri</h3>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="https://www.linkedin.com/in/sudhanshu-p-43797ab1/" target="_blank" className="flex justify-center items-center gap-4">
+                        <Linkedin />
+                        <h3 className="hidden lg:block">Sudhanshu Panthri</h3>
+                    </Link>
+                </div>
+                <div >
+                    <Link href="/" target="_blank" className="flex justify-center items-center gap-4">
+                        <Mail />
+                        <h3 className="hidden lg:block">panthrisudhanshu666@gmail.com</h3>
+                    </Link>
+                </div>
+                <Button className="rounded-3xl font-normal hover:transition-all hover:scale-[1.1]">Contact</Button>
             </div>
         </div>
     )
